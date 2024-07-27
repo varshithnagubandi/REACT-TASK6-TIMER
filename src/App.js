39cb743c707a8f62bc06b07d00977de2024136a1
vvvarshith <vvvarshith.nagubandi@gmail.com>
2024-07-27 18:53:48 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(60);
+  useEffect(() => {
+    if (time > 0) {
+      setTimeout(() => setTime(time - 1), 1000);
+    }
+  }, [time]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>Time Left : {time} seconds</h4>
     </div>
   );
 }
